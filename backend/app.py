4349f344a29b159ccccf187152b3e49cc267f6c6
Rupesh Sharma <102rupeshsharma@gmail.com>
2025-05-26 16:15:19 +0530

    made minior change in app.py file

--- backend/app.py
+++ backend/app.py
@@ -114,5 +114,5 @@ def profile(current_user):
     }), 200
 
 # -------------------- Main --------------------
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
